b939a52a8c78e2f547e3da9d1de2923bc17fa0ab
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-03-13 21:27:59 +0100

    Player sprite flips according to running direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 10f;
+
+    Rigidbody2D myRigidBody2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody2D.velocity.y);
+        myRigidBody2D.velocity = playerVelocity;
         
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        bool runningHorizontally = Mathf.Abs(myRigidBody2D.velocity.x) > Mathf.Epsilon;
+
+        if(runningHorizontally)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody2D.velocity.x), 1f);
+        }
     }
 }
