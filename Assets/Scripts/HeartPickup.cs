fb1d4ca599bacee9941b23e55b8a982baaa44446
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-03-16 11:14:30 +0100

    Lame sound effects added to pickups

--- Assets/Scripts/HeartPickup.cs
+++ Assets/Scripts/HeartPickup.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class HeartPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip heartPickupSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(heartPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
